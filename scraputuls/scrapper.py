b046ca8aa74af3f283b75001b5f7ae6a13dbacf4
Mayonnaise-Slap <368758@niuitmo.ru>
2024-02-06 16:12:31 +0300

    added tag scrapper

--- scraputuls/scrapper.py
+++ scraputuls/scrapper.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import datetime
-from db import habr_news, session
 import multiprocessing as mp
 
 
@@ -20,28 +19,78 @@ def get_soup(page_link: str) -> BeautifulSoup:
     return BeautifulSoup(r.text, "html.parser")
 
 
-def get_article_text(article_link: str) -> str:
+def get_article_text(article: BeautifulSoup) -> str:
     """
-    This function takes a link to an article on habr and
+    This function takes a soup of an article on habr and
     returns the article in plain text. It does not save
     pictures or links, only plain text is returned.
     Args:
-        article_link (): A link to a habr article
+        article: A link to a habr article
 
     Returns:
     text (str): The article's plain text
     """
-    response = requests.get(article_link)
-    if response.status_code != 200:
-        raise Exception("foobar")
-    soup = BeautifulSoup(response.text, "html.parser")
 
     return "\n".join(
         tag_contents.text.replace("&shy", "")
         .replace(";\xad", "")
         .replace("\xad", "")
         .replace("\xa0", " ")
-        for tag_contents in soup.find(
+        for tag_contents in article.find(
             "div", {"class": "article-formatted-body"}
         ).contents
     )
+
+
+def get_article_attrs(article: BeautifulSoup) -> dict:
+    """
+    function takes a soup of an article on habr and
+    returns the article's attributes that are listed
+    on the top.
+    Args:
+        article (): soup of an article on habr
+
+    Returns:
+    dict with article attributes in a form
+    {
+    'Уровень сложности': 'Простой',
+    'Время на прочтение': '8 мин',
+    'Количество просмотров  69K': '69K',
+    'other traits':
+            ['Работа с видео',
+            'Машинное обучение',
+            'Искусственный интеллект',
+            'Звук']
+            }
+    """
+    article_tags = [
+        n.text.strip(" *") for n in article.find_all(
+            "a", {
+                "class": "tm-publication-hub__link"
+            })
+    ]
+    traits_soup = article.find_all(
+        "div", {
+            "class": "tm-article-snippet__stats"
+        }
+    )[0].find_all("span")
+
+    trait_map = dict()
+
+    for trait_id in range(len(traits_soup) // 2):
+        trait = traits_soup[trait_id*2].title.text.strip()
+        value = traits_soup[trait_id*2+1].text.strip()
+
+        trait_map[trait] = value
+
+    trait_map["tags"] = article_tags
+
+    return trait_map
+
+
+def scrape_article():
+    """
+        should return the article text, attributes and url's
+        to the author's and commentators profiles
+    """
+    pass
