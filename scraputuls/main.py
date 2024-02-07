1c3715ea7149b75badf30267fd7dbc8963b4588b
Mayonnaise-Slap <368758@niuitmo.ru>
2024-02-07 20:00:52 +0300

    updated hub scraping stuff

--- scraputuls/main.py
+++ scraputuls/main.py
@@ -1,14 +1,26 @@
-from scrape_hub import get_page_urls
+from scrape_hub import get_page_urls, get_number_of_pages, get_soup
 import multiprocessing as mp
+from itertools import chain
+
+
+def scrape_hub(url: str, n_pages: int = None) -> list:
+    hub_soup = get_soup(url)
+    url += "page{}/"
+
+    if n_pages is None:
+        n_pages = get_number_of_pages(hub_soup)
+
+    parameters = list(url.format(i + 1) for i in range(n_pages))
+    pool = mp.Pool()
+    pool_result = pool.map_async(get_page_urls, parameters)
+    pool.close()
+    pool.join()
+
+    return list(chain.from_iterable(pool_result.get()))
 
 
 if __name__ == '__main__':
-    url = "https://habr.com/ru/hubs/machine_learning/articles/top/alltime/page{}/"
-    params = list(url.format(i + 1) for i in range(2))
+    url = "https://habr.com/ru/hubs/machine_learning/articles/top/alltime/"
 
-    p = mp.Pool()
-    a = p.map(get_page_urls, params)
-    p.close()
-    p.join()
+    print(scrape_hub(url), 3)
 
-    print(a)
--- scraputuls/scrape_hub.py
+++ scraputuls/scrape_hub.py
@@ -1,6 +1,5 @@
 from scrapper import get_soup
 from bs4 import BeautifulSoup
-import multiprocessing as mp
 
 
 def get_number_of_pages(hub_soup: BeautifulSoup) -> int:
@@ -12,7 +11,10 @@ def get_number_of_pages(hub_soup: BeautifulSoup) -> int:
 
 
 def get_page_urls(hub_page_link: str) -> list[str]:
-    hub_soup = get_soup(hub_page_link)
+    try:
+        hub_soup = get_soup(hub_page_link)
+    except Exception:
+        return []
     base_url = 'https://habr.com'
 
     data_links = list()
@@ -24,24 +26,4 @@ def get_page_urls(hub_page_link: str) -> list[str]:
     return data_links
 
 
-def scrape_hub(url: str) -> list[str]:
-    n_pages = get_number_of_pages(get_soup(url))
-    url += "page{}/"
-
-    results = list()
-
-    pool = mp.Pool()
-
-
-    get_page_urls(
-        get_soup(
-            url.format(1)
-        )
-    )
-
-    return results
-
-
-if __name__ == '__main__':
-    scrape_hub("https://habr.com/ru/hubs/machine_learning/articles/top/alltime/")
-
+link = "https://habr.com/ru/hubs/machine_learning/articles/top/alltime/"
