5cd8bce4dcc33c9e1974747dfdab43d3de17d8e5
Mayonnaise-Slap <368758@niuitmo.ru>
2024-02-07 12:17:34 +0300

    a rough scrapper of profile info. should probably test on more accounts

--- scraputuls/scrapper.py
+++ scraputuls/scrapper.py
@@ -100,6 +100,37 @@ def get_comments_accounts(article_link: str) -> tuple:
     )
 
 
-def scrape_profiles(profile_link: str) -> dict:
-    # TODO scrape profile
-    pass
+def scrape_profile(profile_link: str) -> dict:
+    profile_soup: BeautifulSoup = get_soup(profile_link)
+
+    data = dict()
+
+    data["tag"] = profile_soup.find(
+        "a", {
+            "class": "tm-user-card__nickname"
+        }
+    ).text.strip()
+    data["karma"] = int(profile_soup.find("div", {"class": "tm-karma__votes"}).text)
+    data["rating"] = float(profile_soup.find("span", {"class": "tm-votes-lever__score-counter"}).text)
+
+    vals = profile_soup.find_all("dd")
+
+    for i, tag in enumerate(profile_soup.find_all("dt")):
+        match tag.string.strip():
+            case "Специализация":
+                data[tag.string.strip()] = {
+                    vals[i].find("strong").string: list(
+                        i.text for i in vals[i].find_all("div", {"class": "tm-user-specialization__skill"}))}
+            case "Состоит в хабах":
+                data[tag.string.strip()] = list(i.text for i in vals[i].find_all("a", {"class": "tm-user-hubs__hub"}))
+            case "Подписан на компании":
+                data[tag.string.strip()] = list(
+                    i.text for i in vals[i].find_all("a", {"class": "tm-company-snippet__title"}))
+            case "Дата рождения" | "Зарегистрирован":
+                data[tag.string.strip()] = vals[i].find("time")["title"].split(", ")[0]
+            case "Активность" | "Контактная информация" | "Приглашен":
+                pass
+            case _:
+                data[tag.string.strip()] = vals[i].text.strip().replace("\xa0", " ")
+
+    return data
